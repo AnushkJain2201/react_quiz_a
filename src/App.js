5bfddc5c86c99ce8437c734791b491cd8100ab12
Anushk Jain <anushk2201@gmail.com>
2024-01-17 09:42:47 +0530

    feat: started the react app quiz and added the fake server.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+
+// WE CAN CREATE A FAKE SERVER BU USING AN NPM PACKAGE JSON-SERVER
+import Header from "./Header";
+import Main from "./Main";
 
 function App() {
-  return (
-    <div>
-      <DateCounter />
-    </div>
-  );
+	return (
+		<div className='app'>
+			{/* <DateCounter /> */}
+			<Header />
+
+            <Main>
+                <p>1/15</p>
+                <p>Question?</p>
+            </Main>
+		</div>
+	);
 }
 
 export default App;
